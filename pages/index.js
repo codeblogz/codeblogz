20a32a6b4afc28770025b15368e6939c6fb37294
iamrealleader <sabircg125@gmail.com>
2023-04-29 15:44:40 +0500

    checking

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,59 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const[blogs,setBlogs] = useState([]);
+
+  useEffect( ()=>{
+    async function fetchadata(){
+      let res = await fetch(
+       "http://localhost:3000/api/getblogs?catagorie=all&page=1",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      let data = await res.json();
+      console.log(data);
+    }
+    fetchadata();
+  },[]);
+
   return (
+    <>
    <div className="text-5xl w-full h-full flex items-center justify-center bg-black text-white">hello world!</div>
+   {blogs && blogs.length > 0 ? (
+    blogs.map((blog) => {
+      return (
+          <div className="w-[20rem] mx-auto" id={blog._id}>
+           
+             <Image unoptimized width={100} height={100} className="w-[20rem] h-[13rem] object-cover" src={blog.url} alt={blog.title}></Image>
+            <h4 id={blog.catagorie} className="catagorie">
+              {blog.catagorie}
+            </h4>
+            <h1 className="title font-bold text-xl">
+              {blog.title.length > 20
+                ? blog.title.substring(0, 20) + "..."
+                : blog.title}
+            </h1>
+            <p className="break-words description font-semibold">
+              {blog.description.length > 25
+                ? blog.description.substring(0, 25) + "..."
+                : blog.description}
+            </p>
+          </div>
+      );
+    })
+  ) : (
+    <div className="error text-red-600 font-serif font-bold">
+      No blog Aviable
+    </div>
+  )}
+  </>
   )
 }
---
+++ schemas/Blog.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const blogSchema  = new mongoose.Schema({
+            email:{
+                type : String,
+                required : true,
+            },
+            auther:{
+                type : String,
+                required : true,
+            },
+            url :{
+                type : String,
+                default : "insert"
+            },
+            title:{
+                type : String,
+                required : true,
+            },
+            description:{
+                type : String,
+                required : true,
+            },
+            catagorie:{
+                type : String,
+                default : "All"
+            }
+        },
+        {
+            timestamps : true
+        }
+        );
+
+const Blog = mongoose.models.Blog || mongoose.model("Blog",blogSchema);
+export default Blog;
